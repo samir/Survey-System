ba6d824d95ac43c6aecf97f405535996427a11e1
Samir Braga <samirbraga@gmail.com>
2011-07-31 00:32:18 -0300

    configurando rota das paginas estaticas

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 SurveySystem::Application.routes.draw do
 
-  get "users/new"
 
+  get "users/new"
   get "users/create"
 
   # Static pages
-  get "home/about"
+  get "about"   => "pages#about",   :as => :about
+  get "privacy" => "pages#privacy", :as => :privacy
+  get "tos"     => "pages#tos",     :as => :tos
+  get "faq"     => "pages#faq",     :as => :faq
+  get "contact" => "pages#contact", :as => :contact
 
   # Sessions routes
   get "logout" => "sessions#destroy", :as => "logout"
-  get "login" => "sessions#new", :as => "login"
+  match "login" => "sessions#new", :via => [:get, :post], :as => "login"
   get "signup" => "users#new", :as => "signup"
 
   root :to => "home#index"
