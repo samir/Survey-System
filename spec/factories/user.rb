976578b9a28588cf042a4bfeafeeb711f77319f0
Samir Braga <samirbraga@gmail.com>
2011-08-03 21:56:14 -0300

    correcao na fixture de user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,6 +2,6 @@ Factory.define :user do |u|
   u.sequence(:name) {|n| "User Name ##{n}"}
   u.sequence(:email) {|n| "user#{n}@example.com"}
   u.password "123"
-  # u.avatar File.open("#{Rails.root}/tmp/seed/user1.jpg")
+  # u.avatar { File.open("#{Rails.root}/tmp/seed/user1.jpg") }
   u.password_digest "$2a$10$JotYAjf8TLNiLSDYklhspOzgMV9hwLE05SeYXrWb4v8ESejYTttLa" # digest for 123
 end
